05f44dc45570b81104af921b2f911a9eafe267c8
Siddhantham Naveen <snaveen6281@gmail.com>
2026-02-07 09:22:42 +0530

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT for GitHub Pages
+  base: "/Naveen-s-Portfolio/",
+
   css: {
     modules: {
       localsConvention: "camelCase",
